c1d720aa831b52085719115b8c06c66133cca308
신정환 <jhhhh@sinjeonghwan-ui-MacBookAir.local>
2022-02-18 01:35:10 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
+  // formStyle = {
+  //   paddingBottom: 20,
+  //   borderRadius: "30px 15px"
+  // };
+
+  // divStyle = {
+  //   backgroundColor: "yellow",
+  //   // position: "absolute",
+  //   // top: "50%",
+  //   // left: "50%",
+  //   // transform: [
+  //   //   "-50%", "-50%"
+  //   // ],
+  // };
+  
+
+  render() {
+    return (
+        <div class="search-box">
+          <form>
+            <input type="text" placeholder="Enter Codeforces Handle"/>
+          </form>
+        </div>
+    );
+  }
+
+}
+
+export default App;
