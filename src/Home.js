104a56fcff2b79e571f92020a6f9f85878a895fd
신정환 <jhhhh@sinjeonghwan-ui-MacBookAir.local>
2022-02-24 19:42:30 +0900

    api set

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,51 @@
 import React from 'react';
 import HandleSearchBar from './Components/HandleSearchBar';
+import Axios from "axios";
 
 class Home extends React.Component {
+    state = {
+        userHandle: "",
+        userStatusData: [],
+        userRatingData: [],
+    }
+
+    getUserData = (userHandle) => {
+        const base = "https://codeforces.com/api/user.status?handle=";
+        Axios.get(base + userHandle)
+            .then(
+                (res) => {
+                    this.setState({
+                        searchData: res.data.result,
+                        userHandle: userHandle
+                    })
+                }
+            )
+            .catch(
+                (error) => {
+                    this.setState({
+                        error: true,
+                    })
+                    console.log(this.state.error);
+                }
+            );
+
+        Axios.get(base + userHandle)
+            .then(
+                (res) => {
+                    this.setState({
+                        userRatingData: res.data.result
+                    })
+                }
+            )
+            .catch(
+                (error) => {
+                    this.setState({
+                        error: true,
+                    })
+                    console.log(this.state.error);
+                }
+            );
+    }
 
 
     render() {
