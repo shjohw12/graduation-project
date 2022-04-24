e7ba094399719a6c268d50b4f2bea5904549b972
신정환 <jhhhh@sinjeonghwan-ui-MacBookAir.local>
2022-04-24 19:13:25 +0900

    add tag chart

--- src/Components/Charts/TagChart.js
+++ src/Components/Charts/TagChart.js
@@ -31,9 +31,70 @@ class TagChart extends React.Component {
             data.push([elem, tagObj[elem]]);
         }
 
+        const colors = [
+            '#f44336',
+            '#E91E63',
+            '#9C27B0',
+            '#673AB7',
+            '#2196F3',
+            '#009688',
+            '#8BC34A',
+            '#CDDC39',
+            '#FFC107',
+            '#FF9800',
+            '#FF5722',
+            '#795548',
+            '#607D8B',
+            '#E65100',
+            '#827717',
+            '#004D40',
+            '#1A237E',
+            '#6200EA',
+            '#3F51B5',
+            '#F50057',
+            '#304FFE',
+            '#b71c1c',
+        ]
+
+        const title = 'Tag of ' + this.props.userHandle;
 
         return (
             <div>
+                {
+                    len > 0 &&
+                    <Chart
+                        width={'100%'}
+                        height={'500px'}
+                        className="col s12 z-depth-2"
+                        chartType="PieChart"
+                        data={data}
+                        options={{
+                            legend: {
+                                position: 'right',
+                                alignment: 'center',
+                                textStyle: {
+                                    fontSize: 12,
+                                    fontName: 'Roboto'
+                                }
+                            },
+                            chartArea: { width: '80%', height: '70%' },
+                            title: title,
+                            pieSliceText: 'label',
+                            pieHole: 0.5,
+                            tooltip: {
+                                text: 'percentage'
+                            },
+                            fontName: 'Roboto',
+                            titleTextStyle: {
+                                fontSize: 18,
+                                color: '#393939',
+                                bold: false,
+                            },
+                            colors: colors
+                        }}
+                        rootProps={{ 'data-testid': '2' }}
+                    />
+                }
 
             </div>
         )
